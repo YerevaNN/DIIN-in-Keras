82600500a19f8143ca87a5f2fa1c6ed158f5cbd7
MartinXPN <mirakyanmartin@gmail.com>
2018-01-13 12:38:32 +0400

    moved l2 regularization functions inside BaseL2Optimizer

--- optimizers/l2util.py
+++ optimizers/l2util.py
@@ -6,34 +6,6 @@ from keras import backend as K
 from keras.optimizers import Optimizer
 
 
-def compute_l2_ratio(time, l2_full_step, l2_full_ratio):
-    return K.sigmoid((time - l2_full_step / 2) * 8 / (l2_full_step / 2)) * l2_full_ratio
-
-
-def compute_decaying_l2_loss(loss, params, time, l2_full_step, l2_full_ratio):
-    """ Add exponential decaying L2 regularization to all weights in the network """
-    l2_ratio = compute_l2_ratio(time, l2_full_step, l2_full_ratio)
-    for w in params:
-        loss += K.sum(l2_ratio * K.square(w))
-    return loss
-
-
-def compute_difference_l2_loss(loss, params, l2_difference_ratio):
-    """ Add L2 regularization to penalize the difference between weights """
-    penalize_difference = {}
-    for param in params:
-        if getattr(param, 'penalize_difference', None) is not None:
-            weight_name = param.penalize_difference
-            if weight_name not in penalize_difference:
-                penalize_difference[weight_name] = []
-            penalize_difference[weight_name].append(param)
-
-    for penalize_weights in penalize_difference.values():
-        for w1, w2 in itertools.combinations(penalize_weights, 2):
-            loss += K.sum(l2_difference_ratio * K.square(w1 - w2))
-    return loss
-
-
 class BaseL2Optimizer(Optimizer):
 
     def __init__(self,
@@ -49,10 +21,34 @@ class BaseL2Optimizer(Optimizer):
     def get_updates(self, loss, params):
         return NotImplementedError
 
+    @staticmethod
+    def compute_l2_ratio(time, l2_full_step, l2_full_ratio):
+        return K.sigmoid((time - l2_full_step / 2) * 8 / (l2_full_step / 2)) * l2_full_ratio
+
+    def add_decaying_l2_loss(self, loss, params, time):
+        """ Add exponential decaying L2 regularization to all weights in the network """
+        l2_ratio = self.compute_l2_ratio(time, self.l2_full_step, self.l2_full_ratio)
+        for w in params:
+            loss += K.sum(l2_ratio * K.square(w))
+
+    def add_difference_l2_loss(self, loss, params):
+        """ Add L2 regularization to penalize the difference between weights """
+        penalize_difference = {}
+        for param in params:
+            if getattr(param, 'penalize_difference', None) is not None:
+                weight_name = param.penalize_difference
+                if weight_name not in penalize_difference:
+                    penalize_difference[weight_name] = []
+                penalize_difference[weight_name].append(param)
+
+        for penalize_weights in penalize_difference.values():
+            for w1, w2 in itertools.combinations(penalize_weights, 2):
+                loss += K.sum(self.l2_difference_full_ratio * K.square(w1 - w2))
+
     def get_l2_loss(self, loss, params, iterations):
         iterations = K.cast(iterations, dtype='float32')
-        loss = compute_decaying_l2_loss(loss, params, iterations, self.l2_full_step, self.l2_full_ratio)
-        loss += compute_difference_l2_loss(loss, params, self.l2_difference_full_ratio)
+        self.add_decaying_l2_loss(loss, params, iterations)
+        self.add_difference_l2_loss(loss, params)
         return loss
 
     def get_config(self):
